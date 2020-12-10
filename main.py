4daf6629c4900fc2c06dd7154fa240b550890112
chalupkar <chalupkar@gmail.com>
2020-12-10 16:11:42 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def on_forever():
-    pass
+    basic.show_string("RASTO")
+    basic.show_icon(IconNames.GHOST)
+    basic.show_icon(IconNames.GHOST)
+    basic.pause(3000)
+    basic.show_icon(IconNames.GIRAFFE)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,5 +2,6 @@ basic.forever(function () {
     basic.showString("RASTO")
     basic.showIcon(IconNames.Ghost)
     basic.showIcon(IconNames.Ghost)
+    basic.pause(3000)
     basic.showIcon(IconNames.Giraffe)
 })
